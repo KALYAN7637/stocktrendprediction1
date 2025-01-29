30647fb3950a30a116a45d2c55073d1e60c32c42
NALAMKALYANACHAKRAVARTHI <100521699+KALYAN7637@users.noreply.github.com>
2025-01-29 22:40:46 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -89,6 +89,80 @@ data_training_scaled = scaler.fit_transform(data_training.values.reshape(-1, 1))
 
 #print(data_training.shape)
 #print(data_testing.shape)
+from sklearn.preprocessing import MinMaxScaler
+scaler=MinMaxScaler(feature_range=(0,1))
+
+data_training_array= scaler.fit_transform(data_training) 
+
+x_train=[]
+y_train=[]
+
+for i in range(100,data_training_array.shape[0]):
+    x_train.append(data_training_array[i-100:i])
+    y_train.append(data_training_array[i,0])
+
+x_train,y_train=np.array(x_train),np.array(y_train)
+
+model=load_model(r'keras_model.h5')
+
+
+past_100_days=data_training.tail(100)
+final_df=past_100_days.append(data_testing, ignore_index=True)
+input_data=scaler.fit_transform(final_df)
+
+x_test=[]
+y_test=[]
+
+for i in range(100,input_data.shape[0]):
+    x_test.append(input_data[i-100:i])
+    y_test.append(input_data[i,0])
+
+
+x_test,y_test=np.array(x_test),np.array(y_test)
+y_predicted=model.predict(x_test)
+scaler.scale_
+
+scale_factor=1/scaler.scale_[0]
+y_predicted=y_predicted*scale_factor
+y_test=y_test*scale_factor
+
+
+
+
+
+
+
+
+
+
+
+
+#final graph
+
+
+st.subheader('Prediction vs Original Price')
+plt2=plt.figure(figsize=(12,6))
+plt.plot(y_predicted,'r',label='Predicted Price')
+plt.plot(y_test,'b',label='Original Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(plt2)
+plt.show()
+
+
+
+
+
+#pricing_df, fundamental_df, news=st.tabs(['Pricing Data','Fundamental Data','Top 10 News'])
+
+st.subheader('Price Movements')
+df2=df
+df2['% Change']=df['Close']/df['Close'].shift(1) - 1
+df2.dropna(inplace = True)
+st.write(df)
+annual_return=df2['% Change'].mean()*252*100
+st.write('Annual Return is', annual_return,'%')
 
 
 
